7f0a8e6cc2b469d4fc3ecca56cab65ba348e886c
lvsf <lvsf@163.com>
2020-06-02 16:53:07 +0800

    完成小案例

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,73 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    inputValue: 'aaa',
+    id: 5,
+    viewIndex: 0
   },
   mutations: {
+    initList (state, data) {
+      state.list = data
+    },
+    bindCode (state, data) {
+      state.inputValue = data
+    },
+    addItem (state) {
+      const obj = {
+        id: state.id,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.inputValue = ''
+      state.id++
+    },
+    deleteItem (state, flag) {
+      const i = state.list.findIndex(x => x.id === flag)
+      if (i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    chooseItem (state, flag) {
+      const i = state.list.findIndex(x => x.id === flag)
+      if (i !== -1) {
+        state.list[i].done = !state.list[i].done
+      }
+    },
+    cleanDone (state) {
+      state.list = state.list.filter(x => x.done === false)
+    },
+    changeStatus (state, flag) {
+      state.viewIndex = flag
+    }
   },
   actions: {
+    getList (context) {
+      axios.get('/list.json').then(res => {
+        context.commit('initList', res.data)
+      })
+    }
+  },
+  getters: {
+    unDoneLength (state) {
+      return state.list.filter(x => x.done === false).length
+    },
+    handleList (state) {
+      if (state.viewIndex === 0) {
+        return state.list
+      } else if (state.viewIndex === 1) {
+        return state.list.filter(x => x.done === false)
+      } else if (state.viewIndex === 2) {
+        return state.list.filter(x => x.done === true)
+      }
+      return state.list
+    }
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,100 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <a-input placeholder="请输入任务" class="my_ipt" :value="inputValue" @change="getValue"/>
+<!--    <a-input placeholder="请输入任务" class="my_ipt" v-model="defaultValue" @change="getValue"/>-->
+    <a-button type="primary" @click="addItems">添加事项</a-button>
+
+    <a-list bordered :dataSource="handleList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked="item.done" @change="checkItem(item.id)">{{item.info}}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="removeItem(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div slot="footer" class="footer">
+        <!-- 未完成的任务个数 -->
+        <span>{{unDoneLength}}条剩余</span>
+        <!-- 操作按钮 -->
+        <a-button-group>
+          <a-button :type="viewIndex == 0 ? 'primary': 'default'" @click="changeBtn(0)">全部</a-button>
+          <a-button :type="viewIndex == 1 ? 'primary': 'default'" @click="changeBtn(1)">未完成</a-button>
+          <a-button :type="viewIndex == 2 ? 'primary': 'default'" @click="changeBtn(2)">已完成</a-button>
+        </a-button-group>
+        <!-- 把已经完成的任务清空 -->
+        <a @click="clean">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import { mapState, mapGetters } from 'vuex'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      defaultValue: ''
+    }
+  },
+  created () {
+    this.$store.dispatch('getList')
+  },
+  methods: {
+    // 获取搜索框的值
+    getValue (e) {
+      this.$store.commit('bindCode', e.target.value)
+    },
+    // 添加一项
+    addItems () {
+      if (this.inputValue.trim().length < 1) {
+        return this.$message.warning('不能为空')
+      }
+      this.$store.commit('addItem', this.inputValue)
+    },
+    // 删除
+    removeItem (id) {
+      this.$store.commit('deleteItem', id)
+    },
+    // 选中值
+    checkItem (id) {
+      this.$store.commit('chooseItem', id)
+    },
+    // 清空已完成
+    clean () {
+      this.$store.commit('cleanDone')
+    },
+    // 切换按钮
+    changeBtn (flag) {
+      this.$store.commit('changeStatus', flag)
+    }
+  },
+  computed: {
+    ...mapState(['inputValue', 'viewIndex']),
+    ...mapGetters(['unDoneLength', 'handleList'])
   }
 }
 </script>
+
+<style scoped>
+  #app {
+    padding: 10px;
+  }
+
+  .my_ipt {
+    width: 500px;
+    margin-right: 10px;
+  }
+
+  .dt_list {
+    width: 600px;
+    margin: 10px auto;
+  }
+
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
